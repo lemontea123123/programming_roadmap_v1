d51c9af6cbc72733a1160ab38db01414aa40b3c2
Raymond T <lemondtea123@gmail.com>
2026-01-03 12:49:47 +0700

    Completed Level 4

--- v1/Level 4/flask_level_4_hello/app_test_1.py
+++ v1/Level 4/flask_level_4_hello/app_test_1.py
@@ -14,6 +14,10 @@ o	Ambil data dari request.form['judul'] dan request.form['isi'], lalu print di t
 from flask import Flask , render_template , request
 from werkzeug.exceptions import BadRequestKeyError
 from flask import jsonify
+from flask import redirect
+from flask import url_for
+
+last_id = 3
 
 list_movie = [
     {
@@ -33,11 +37,52 @@ list_movie = [
     }
 ]
 
+def add_movie(movie_name,movie_content):
+    global last_id
+
+    new_id = last_id + 1
+    last_id = new_id
+    new_movie_dict = {
+        "id": new_id ,
+        "title":movie_name,
+        "content":movie_content
+    }
+    list_movie.append(new_movie_dict)
+
+def del_movie(movie_id):
+    target_index = -1
+
+    list_movie_indexes = enumerate(list_movie,start=0)
+    for index , movie_dict in list_movie_indexes:
+        current_movie_id = movie_dict["id"]
+        if current_movie_id == movie_id :
+            target_index = index
+            break
+    
+    if(target_index == -1):
+        print("Movie ID Out Of Range")
+        return -1
+    
+    list_movie.pop(target_index)
+    return target_index
+    
 app = Flask(__name__)
 
 @app.route("/")
 def show_form_index_html():
-    return render_template("index.html")
+    return render_template("index.html",list_movie=list_movie)
+
+@app.route("/add",methods=["GET","POST"])
+def add_refresh_index_html():
+    try :
+        title = request.form["title"]
+        content = request.form["content"]
+    except BadRequestKeyError:
+        return render_template("index.html",error="Please Fill In Information",list_movie=list_movie)
+    
+    add_movie(title,content)
+
+    return render_template("index.html",list_movie=list_movie)
 
 @app.route("/submit",methods=["GET","POST"])
 def show_result_result_html():
@@ -49,6 +94,14 @@ def show_result_result_html():
 
     return render_template("result.html",title=title,content=content)
 
+@app.route("/delete/<int:id>")
+def delete_and_refresh_index_html(id):
+    del_movie(id)
+    #return render_template("index.html",list_movie=list_movie)
+    return redirect(url_for("show_form_index_html"))
+
+
+
 @app.route("/api/notes",methods=["GET"])
 def return_notes_list():
     return jsonify(list_movie)
@@ -58,10 +111,28 @@ def add_note():
     note_obj = request.get_json()
 
     print("Received JSON:\n",note_obj)
+
+    title = note_obj["title"]
+    content = note_obj["content"]
+
+    add_movie(title,content)
+
     return {
         "status":"ok"
     }
 
+@app.route("/api/notes/<int:id>",methods=["DELETE"])
+def api_delete_note(id):
+    if(del_movie(id) != -1 ):
+        response = {
+            "status":"deleted"
+        }
+    else:
+        response = {
+            "status":"ID Not Found"
+        }
+
+    return response
 
 if(__name__ == "__main__"):
     app.run(debug=True)
--- v1/Level 4/flask_level_4_hello/test.py
+++ v1/Level 4/flask_level_4_hello/test.py
@@ -6,6 +6,8 @@ Testing Flask
 
 from flask import Flask
 
+last_id = 3
+
 list_movie = [
     {
         "id":1,
@@ -24,15 +26,41 @@ list_movie = [
     }
 ]
 
-list_kosong = []
+def add_movie(movie_name,movie_content):
+    global last_id
 
-def get_last_index(list):
-    last_index = len(list)-1
+    new_id = last_id + 1
+    last_id = new_id
+    new_movie_dict = {
+        "id": new_id ,
+        "title":movie_name,
+        "content":movie_content
+    }
+    list_movie.append(new_movie_dict)
 
-    if(last_index > 0):
-        last_movie_id = list[last_index]["id"]
-        return last_movie_id
-    else:
-        return 0
+def del_movie(movie_id):
+    target_index = -1
 
-print(get_last_index(list_movie))
+    list_movie_indexes = enumerate(list_movie,start=0)
+    for index , movie_dict in list_movie_indexes:
+        current_movie_id = movie_dict["id"]
+        if current_movie_id == movie_id :
+            target_index = index
+            break
+    
+    if(target_index == -1):
+        print("Movie ID Out Of Range")
+        return
+    
+    list_movie.pop(target_index)
+    
+
+
+
+
+print("list movie:\n",list_movie,"\nLast ID:",last_id,"\n")
+add_movie("Fatman","Film tentang Fatman")
+print("list movie:\n",list_movie,"\nLast ID:",last_id,"\n")
+
+del_movie(1)
+del_movie(2)

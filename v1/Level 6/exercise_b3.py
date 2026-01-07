9d3ccd6934170b12886f72dad3371ef2178c0fb9
Raymond T <lemondtea123@gmail.com>
2026-01-07 16:23:19 +0700

    Level 6 exercise_b4

--- v1/Level 6/exercise_b3.py
+++ v1/Level 6/exercise_b3.py
@@ -20,14 +20,13 @@ o	Print juga berapa banyak iterasi yang dilakukan untuk membandingkan dengan bru
 list_angka = [3, 7, 1, 9, 2, 6, 0, 8, 4, 5]
 
 def two_sum_brute(list_angka,target):
-    total_iterasi = 0
+    
     result_tuple = None
 
     for i1 , angka_1 in enumerate(list_angka,0):
         current_angka = angka_1
         for i2 ,angka_2 in enumerate(list_angka):
-            total_iterasi += 1
-            print("total_iterasi = ",total_iterasi)
+            
 
             if(i1 == i2):
                 continue
@@ -47,36 +46,20 @@ def two_sum_hash(list_angka,target):
     #kombinasi_valid = []
 
     for i1 , angka in enumerate(list_angka):
-        total_iterasi += 1
-        print("total_iterasi = ",total_iterasi)
-        try:
-            #Jika angka sudah pernah di indeks
-            new_dict[angka].append(i1)
-        except KeyError:
-            #Jika angka belum pernah di indeks
-            new_dict[angka] = [i1]
-        
+
         angka_dicari = target - angka
-        list_i_angka_dicari = []
+        
+        if angka_dicari in new_dict:
+            return (i1,new_dict[angka_dicari][0])
+        
+        if angka in new_dict:
+            new_dict[angka].append(i1)
+        else:
+            new_dict[angka] =[i1]
+        
+        
 
-        try:
-            #Jika angka_dicari sudh pernah di indeks
-            list_i_angka_dicari = new_dict[angka_dicari]
-        except KeyError:
-            #Jika angka_dicari belum pernah di indeks
-            continue
-
-        #Dari list indeks cariin yg bukan indeks ini
-        # i2 merupakan indeks dari indeks list dari angka_dicari
-        for i2 in list_i_angka_dicari:
-            total_iterasi += 1
-            print("total_iterasi = ",total_iterasi)
-            if(i2!=i1):
-                #Ketemu indeks angka dicari!
-                return (i1,i2)
-                #kombinasi_valid.append((i1,i2))
-
-    #return kombinasi_valid
+    
 
 print(two_sum_brute(list_angka=list_angka,target=17))
 print(two_sum_hash(list_angka=list_angka,target=17))
---
+++ v1/Level 6/exercise_b4.py
@@ -0,0 +1,82 @@
+"""
+Docstring for exercise_b4
+4. Reverse string / list
+Tujuannya mengenal beberapa cara berpikir untuk masalah yang sama.getsdeready+1
+•	Reverse list:
+o	Versi 1: pakai slicing nums[::-1].
+o	Versi 2: pakai loop for i in range(len(nums)-1, -1, -1) dan simpan ke list baru.
+o	Versi 3: pakai two pointers (awal & akhir) lalu swap sampai bertemu di tengah.
+•	Reverse string:
+o	Versi 1: pakai slicing.
+o	Versi 2: convert ke list karakter, reverse manual pakai two pointers, lalu gabung lagi ke string.
+"""
+
+nums2 = [10, 20, 30, 40, 50]
+
+#Versi 1: pakai slicing nums[::-1].
+def reverse_version_1(list_param):
+    new_list = []
+
+    new_list = list_param[::-1]
+
+    return new_list
+
+#Versi 2: pakai loop for i in range(len(nums)-1, -1, -1) dan simpan ke list baru.
+def reverse_version_2(list_param):
+    new_list = []
+
+    for i in range(len(list_param)-1,-1,-1):
+        current_ele = list_param[i]
+        new_list.append(current_ele)
+    return new_list
+
+
+#Versi 3: pakai two pointers (awal & akhir) lalu swap sampai bertemu di tengah
+def reverse_version_3(list_param):
+    
+    list_param_length = len(list_param)
+
+    #Special cases where length is 0
+    if(list_param_length == 0):
+        return list_param
+
+    left_index = 0
+    right_index = list_param_length - 1
+
+    while(right_index > left_index):
+
+        #swap
+        temp = list_param[left_index]
+        list_param[left_index] = list_param[right_index]
+        list_param[right_index] = temp
+
+        left_index = left_index + 1
+        right_index = right_index - 1
+    
+    return list_param
+
+reverse_version_3(nums2)
+        
+
+#Reverse string:
+#o	Versi 1: pakai slicing
+sample_string = "12345"
+
+def reverse_string_slicing(string_param):
+    new_string = sample_string[::-1]
+    return new_string
+
+print(reverse_string_slicing(sample_string))
+
+
+#Versi 2: convert ke list karakter, reverse manual pakai two pointers, lalu gabung lagi ke string.
+def reverse_string_version_2(string_param):
+    sample_list = list(string_param)
+
+    reverse_version_3(sample_list)
+
+    return "".join(sample_list)
+
+result_value = reverse_string_version_2(sample_string)
+
+print("test")
